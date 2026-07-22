bca29cd4900b3dfdff84ef184da45aca2efe5986
Cindy Marin Torres <cindymarintorres@hotmail.com>
2026-07-22 16:59:42 -0500

    feat(config): agregar sictec.php con parametros de la app

---
+++ api/config/sictec.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+return [
+    'api_key' => env('SICTEC_API_KEY'),
+    'rucs_prueba' => [
+        '1790537358001',
+        '0921302998001',
+        '0991331859001',
+        '0914953393001',
+        '0921302998001',
+        '0924901028001',
+        '0967029984001',
+        '0993393045001',
+        '0993040517001',
+        '0921302998001',
+        '0920426798001',
+        '0903655843001',
+        '1790537358001',
+        '0993308056001',
+        '0992881348001',
+    ]
+];
